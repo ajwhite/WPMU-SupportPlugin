7a6114104ee2795a0dd4c41cc7531d1d16775a2f
Atticus White <contact@atticuswhite.com>
2015-08-13 20:15:02 -0400

    page loading

--- lib/init.php
+++ lib/init.php
@@ -19,19 +19,33 @@ function custom_post_types() {
 add_action('init', __NAMESPACE__ . '\\custom_post_types');
 
 function register_site_moderator_menus() {
-  add_menu_page('Support', 'Support', 'manage_options', 'moderator_support_tickets', __NAMESPACE__ . '\\render_mod_page', 'dashicons-format-status');
+  add_menu_page(
+    'Support',
+    'Support',
+    'manage_options',
+    'moderator_support_tickets',
+    __NAMESPACE__ . '\\render_site_moderator_page',
+    'dashicons-format-status'
+  );
 }
 add_action('admin_menu', __NAMESPACE__ . '\\register_site_moderator_menus');
 
 function register_network_admin_menus() {
-  add_menu_page('Support', 'Support', 'manage_options', 'admin_support_tickets', __NAMESPACE__ . '\\render_network_admin_page', 'dashicons-format-status');
+  add_menu_page(
+    'Support',
+    'Support',
+    'manage_options',
+    'admin_support_tickets',
+    __NAMESPACE__ . '\\render_network_admin_page',
+    'dashicons-format-status'
+  );
 }
 add_action('network_admin_menu', __NAMESPACE__ . '\\register_network_admin_menus');
 
-function render_mod_page() {
-  echo 'Mod Page';
+function render_site_moderator_page() {
+  include('views/site_moderator/list.php');
 }
 
 function render_network_admin_page() {
-  echo 'Amdin page';
+  include('views/network_admin/list.php');
 }
---
+++ lib/views/network_admin/list.php
@@ -0,0 +1,6 @@
+<div class="wrap">
+  <h2>Support Tickets</h2>
+  <form method="post">
+
+  </form>
+</div>
